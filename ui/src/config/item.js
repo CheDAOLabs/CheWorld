dcc298ce2c7e934786da461fdde00395acca4dcd
chedao_stuff <chedao_stuff@gmail.com>
2023-10-27 17:42:35 +0800

    1

--- ui/src/config/item.js
+++ ui/src/config/item.js
@@ -1,13 +1,77 @@
-export const composite_config =[
-    {"id":1,"name":21,"icon":"@/assets/images/set1.png","type":2,"subclass":9,"composite":"2|1,7|10,14|10"},
-    {"id":2,"name":26,"icon":"@/assets/images/set1.png","type":1,"subclass":1,"composite":"15|20,8|20,14|20"},
-    {"id":3,"name":21,"icon":"@/assets/images/set1.png","type":1,"subclass":2,"composite":"5|20,16|10"},
-    {"id":4,"name":31,"icon":"@/assets/images/set1.png","type":1,"subclass":3,"composite":"4|20,15|10"},
-    {"id":5,"name":36,"icon":"@/assets/images/set1.png","type":1,"subclass":4,"composite":"4|20,16|20,14|20"},
-    {"id":6,"name":12,"icon":"@/assets/images/set1.png","type":1,"subclass":5,"composite":"4|20,15|20"},
-    {"id":7,"name":41,"icon":"@/assets/images/set1.png","type":1,"subclass":6,"composite":"4|20,5|10"},
-    {"id":8,"name":1,"icon":"@/assets/images/set1.png","type":1,"subclass":7,"composite":"15|20,16|20,17|20"},
-    {"id":9,"name":2,"icon":"@/assets/images/set1.png","type":1,"subclass":8,"composite":"15|20,16|20"},
+export const composite_config = [
+    {
+        "id": "1",
+        "name": "21",
+        "icon": "@/assets/images/set1.png",
+        "type": "2",
+        "subclass": "9",
+        "composite": "2|1,7|10,14|10"
+    },
+    {
+        "id": "2",
+        "name": "26",
+        "icon": "@/assets/images/set1.png",
+        "type": "1",
+        "subclass": "1",
+        "composite": "15|20,8|20,14|20"
+    },
+    {
+        "id": "3",
+        "name": "21",
+        "icon": "@/assets/images/set1.png",
+        "type": "1",
+        "subclass": "2",
+        "composite": "5|20,16|10"
+    },
+    {
+        "id": "4",
+        "name": "31",
+        "icon": "@/assets/images/set1.png",
+        "type": "1",
+        "subclass": "3",
+        "composite": "4|20,15|10"
+    },
+    {
+        "id": "5",
+        "name": "36",
+        "icon": "@/assets/images/set1.png",
+        "type": "1",
+        "subclass": "4",
+        "composite": "4|20,16|20,14|20"
+    },
+    {
+        "id": "6",
+        "name": "12",
+        "icon": "@/assets/images/set1.png",
+        "type": "1",
+        "subclass": "5",
+        "composite": "4|20,15|20"
+    },
+    {
+        "id": "7",
+        "name": "41",
+        "icon": "@/assets/images/set1.png",
+        "type": "1",
+        "subclass": "6",
+        "composite": "4|20,5|10"
+    },
+    {
+        "id": "8",
+        "name": "1",
+        "icon": "@/assets/images/set1.png",
+        "type": "1",
+        "subclass": "7",
+        "composite": "15|20,16|20,17|20"
+    },
+    {
+        "id": "9",
+        "name": "4",
+        "icon": "@/assets/images/set1.png",
+        "type": "1",
+        "subclass": "8",
+        "composite": "15|20,16|20"
+    },
+
 ]
 
 export const item_types = {
