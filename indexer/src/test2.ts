cffe0777bdaab0e3140e951ab20d9fba25bc6c0a
chedao_stuff <chedao_stuff@gmail.com>
2023-11-01 16:01:21 +0800

    1

--- indexer/src/test2.ts
+++ indexer/src/test2.ts
@@ -1,44 +1,28 @@
 import {
-    ADVENTURER_DIED,
-    ADVENTURER_UPGRADED,
-    AMBUSHED_BY_BEAST,
-    ATTACKED_BEAST,
-    ATTACKED_BY_BEAST,
-    DISCOVERED_BEAST,
-    DISCOVERED_GOLD,
-    DISCOVERED_HEALTH,
-    DISCOVERED_XP,
-    DODGED_OBSTACLE,
-    DROPPED_ITEMS,
-    EQUIPPED_ITEMS,
-    FLEE_FAILED,
-    FLEE_SUCCEEDED,
-    HIT_BY_OBSTACLE, ITEMS_LEVELED_UP,
-    NEW_HIGH_SCORE, NEW_ITEMS_AVAILABLE,
-    PURCHASED_ITEMS,
-    PURCHASED_POTIONS,
-    REWARD_DISTRIBUTION,
-    SLAYED_BEAST,
     START_GAME
 } from "./utils/events.ts";
 
 const GAME = Deno.env.get("GAME");
 const START = +(Deno.env.get("START") || 0);
+const AUTH_TOKEN = "dna_Ph4f46VVexs9XUi0qVd6";
 
+console.log(GAME);
+console.log(START);
 
 const filter = {
-    header: { },
+    header: { weak: false },
     events: [
-
+        // { fromAddress: GAME, keys: [START_GAME] },
     ],
 };
-console.log(START);
+
 console.log(filter)
 export const config = {
     streamUrl: "https://goerli.starknet.a5a.ch",
+    token: AUTH_TOKEN,
     filter,
-    startingBlock: START,
-    // startingBlock: 800_000,
+    // startingBlock: START,
+    startingBlock: 894_413,
     network: "starknet",
     finality: "DATA_STATUS_ACCEPTED",
     sinkType: "console",
